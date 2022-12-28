015f5ee64ddfe70c9f260f1c4afc1c5530d4c693
Zachary Mathis <zachamathis@gmail.com>
2022-12-28 15:00:10 -0800

    got the actual logic behind pads working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import * as React from 'react';
-import logo from './logo.svg';
+import Display from './Components/Display';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='drum-machine'>
+      <Display id="display" />
     </div>
   );
 }
