d3d3a90c494184a44137e7b4499be6faaecc1068
Zachary Mathis <zachamathis@gmail.com>
2023-01-05 12:40:00 -0800

    changes the sample layout

--- src/Components/DrumPads.tsx
+++ src/Components/DrumPads.tsx
@@ -7,15 +7,15 @@ const DrumPads: FC<DrumPadProps> = ({setKeyPress, isOn, volume}) => {
     if (isOn) {
         return (
             <div className=" mx-6 my-1 py-12 px-4 grid grid-cols-3 gap-4 w-[275px] bg-slate-500 rounded-lg">
-                <Pad id="Q" src='/audio/Heater-3.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="W" src='/audio/Heater-4.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="E" src='/audio/Heater-1.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="A" src='/audio/Heater-2.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="S" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="D" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="Z" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="X" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
-                <Pad id="C" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="Q" src='/audio/Heater-1.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="W" src='/audio/Heater-2.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="E" src='/audio/Heater-4.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="A" src='/audio/kicknhat.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="S" src='/audio/Heater-3.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="D" src='/audio/ohh.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="Z" src='/audio/kick.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="X" src='/audio/clap.mp3' setKeyPress={setKeyPress} volume={volume} />
+                <Pad id="C" src='/audio/chh.mp3' setKeyPress={setKeyPress} volume={volume} />
             </div>
         )
     } else {
